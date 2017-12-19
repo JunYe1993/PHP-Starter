01483559a6c4da0ed3bd6b8c634a033323f3da1a
JunYe1993 <d486250@gmail.com>
2017-12-19 18:38:04 +0800

    progress : data type

---
+++ SubTest.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+     $num = $_GET['n'];
+     echo "<img src = Pic/MARK0$num.png width = 255 height = 295 />";
+     echo '<br>'.$num;
+?>
+
+</body>
+</html>
--- Test.php
+++ Test.php
@@ -2,27 +2,30 @@
 <html>
 <body>
 
-
-<?php 
-	$h = $_GET["h"];  //取得網址列傳來的變數h 
-	$w = $_GET["w"];  //取得網址列傳來的變數w 
-
-	if($h==""){  //如果$h的內容為空字串 
-		$h = 1.72;  //身高（單位：公尺） 
-	} 
-
-	if($w==""){  //如果$w的內容為空字串 
-		$w = 60;  //體重（單位：公斤） 
-	} 
-
-	echo "您的BMI=".$w/($h*$h);  //輸出您的BMI
-	echo '<br>';
-?> 
+<?php
+	// .在PHP裡扮演連結的效果 data用法(記得改SERVER時區)
+	echo "今天是".date("Y年m月d日 H:i:s").'<br>';
+?>
 
 <?php
-	echo "今天是";
-	echo date("Y年m月d日 H:i:s");
+	$num = rand(1,2);
+	echo "<a href = SubTest.php?n=$num>SportonMark</a>";
+	echo "<img src = Pic/MARK0$num.png width = 255 height = 295 />".'<br>';
 ?>
+
+<form>
+	First name:<br>
+	<input type="text" name="firstname"><br>
+	Last name:<br>
+	<input type="text" name="lastname">
+	<select name = "box">
+		<option value="1">1</option>
+		<option value="2">2</option>
+		<option value="3">3</option>
+		<option value="4">4</option>
+	</select>
+	<input type="submit" name="button" value="push">
+</form>
 
 </body>
 </html>
